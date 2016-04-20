2d553074127ac33c5ec0847e1840ba92f9320fae
Chris Perez <chris.k.perez@gmail.com>
2016-04-20 13:45:21 -0700

    Finished all of the TODOs that deal with the initial page load. Still have to work on TODOs for adding/deleting articles. Also adding fixed geojson code from lecture

--- class-08-crud-a-resource/pair-assignment/starter-code/scripts/article.js
+++ class-08-crud-a-resource/pair-assignment/starter-code/scripts/article.js
@@ -19,10 +19,14 @@
     return template(this);
   };
 
-  // TODO: Set up a DB table for articles.
+  // DONE: Set up a DB table for articles.
   Article.createTable = function(callback) {
     webDB.execute(
-      '...;', // what SQL command do we run here inside these quotes?
+      [
+        {
+          'sql': 'CREATE TABLE IF NOT EXISTS articles (id INTEGER PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), author_url VARCHAR(255), category VARCHAR(255), published_on VARCHAR(255), body VARCHAR(255))'
+        }
+      ], // what SQL command do we run here inside these quotes?
       function(result) {
         console.log('Successfully set up the articles table.', result);
         if (callback) callback();
@@ -30,12 +34,12 @@
     );
   };
 
-  // TODO: Insert an article instance into the database:
+  // DONE: Insert an article instance into the database:
   Article.prototype.insertRecord = function(callback) {
     webDB.execute(
       [
         {
-          'sql': '...;',
+          'sql': 'INSERT INTO articles (title, author, author_url, category, published_on, body) VALUES (?, ?, ?, ?, ?, ?)',
           'data': [this.title, this.author, this.authorUrl, this.category, this.publishedOn, this.body]
         }
       ],
@@ -89,9 +93,11 @@
   // If the DB has data already, we'll load up the data (by descended published order), and then hand off control to the View.
   // If the DB is empty, we need to retrieve the JSON and process it.
   Article.fetchAll = function(next) {
-    webDB.execute('...', function(rows) { // TODO: fill these quotes to 'select' our table.
+    webDB.execute('SELECT * FROM articles', function(rows) { // DONE: fill these quotes to 'select' our table.
       if (rows.length) {
-        // TODO:
+        // DONE:
+        Article.loadAll(rows);
+        next();
         // 1 - Use Article.loadAll to instanitate these rows,
         // 2 - Pass control to the view by calling the next function that was passed in to Article.fetchAll
 
@@ -99,14 +105,18 @@
         $.getJSON('/data/hackerIpsum.json', function(data) {
           // Save each article from this JSON file, so we don't need to request it next time:
           data.forEach(function(obj) {
-            var article = new Article(obj); // This will instantiate an article instance based on each article object from our JSON.
-            // TODO:
+            var article = new Article(obj);
+            Article.all.push(article);
+            article.insertRecord(); // This will instantiate an article instance based on each article object from our JSON.
+            // DONE:
             // 1 - 'insert' the newly-instantiated article in the DB: (hint: what can we call on each 'article' instance?).
 
           });
           // Now get ALL the records out the DB, with their database IDs:
-          webDB.execute('', function(rows) { // TODO: select our now full table
-            // TODO:
+          webDB.execute('SELECT * FROM articles', function(rows) { // DONE: select our now full table
+            // DONE:
+            Article.loadAll(rows);
+            next();
             // 1 - Use Article.loadAll to instanitate these rows,
             // 2 - Pass control to the view by calling the next function that was passed in to Article.fetchAll
 
